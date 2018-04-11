fc91d619a832c2f70a27d0c0e99072df2367cee3
Václav Pavlín <vaclav.pavlin@gmail.com>
2018-04-11 10:33:59 +0200

    Add user/pass generation script

---
+++ generate_users.sh
@@ -0,0 +1,11 @@
+USERS=
+if [ -n "$2" ] || ! [[ "$2" =~ [0-9]* ]]; then
+  USERS=$@
+else
+  USERS=`eval echo user{1..${1}}`
+fi
+
+for u in ${USERS}; do
+  echo -n ${u} : 
+  hashauthpw --length 10 ${u} ${HASH_AUTHETICATOR_SECRET_KEY}
+done
