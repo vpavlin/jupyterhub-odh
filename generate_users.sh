5bd3276f3756be9c1cff96df11399edabc301761
Václav Pavlín <vaclav.pavlin@gmail.com>
2018-04-11 12:06:02 +0200

    Add variabl for pass len

--- generate_users.sh
+++ generate_users.sh
@@ -5,7 +5,11 @@ else
   USERS=`eval echo user{1..${1}}`
 fi
 
+if [ -z "${HASH_AUTHENTICATOR_PASS_LEN}" ]; then
+  HASH_AUTHENTICATOR_PASS_LEN=6
+fi
+
 for u in ${USERS}; do
   echo -n "${u} : "
-  hashauthpw --length 10 ${u} ${HASH_AUTHETICATOR_SECRET_KEY}
+  hashauthpw --length ${HASH_AUTHENTICATOR_PASS_LEN} ${u} ${HASH_AUTHETICATOR_SECRET_KEY}
 done
